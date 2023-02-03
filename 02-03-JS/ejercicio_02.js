5f77057fc78cc9798ff9d6578a8512e271bf316c
DREWiex <aleon88@gmail.com>
2023-02-03 13:59:15 +0100

    Ejercicios JS 03-02

---
+++ 02-03-JS/ejercicio_02.js
@@ -0,0 +1,22 @@
+    let frutas = ["Manzana", "Banana"];
+    console.log(frutas); // Manzana y Banana 
+    console.log(frutas.length); // 2
+    let primero = frutas[0] // Manzana
+    let ultimo = frutas[frutas.length - 1]; // Banana
+    let nuevaLongitud = frutas.push('Naranja'); // Añade "Naranja" al final
+    // ["Manzana", "Banana", "Naranja"]
+    ultimo = frutas.pop(); // Elimina "Naranja" del final
+    // ["Manzana", "Banana"]
+
+
+    function add(){
+        frutas.push("Naranja")
+        alert(frutas);
+        console.log(frutas);
+    }
+
+    function borrar(){
+        frutas.pop();
+        alert(frutas);
+        console.log(frutas)
+    }
